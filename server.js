3c6888b5724b34259cbe65b4c34822bad74d6684
Muhammad-Abid11 <abidkhan11845@gmail.com>
2024-12-16 04:09:21 +0500

    create multiple routes

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const app = express()
 
 const port = process.env.PORT || 3001
 
-
+/*
 app.get('/', (req, res) => {
     // res.send('Hello World')
     res.status(200).json({ message: 'Hello World' })
 }) 
-
-// app.use('/api/contacts',require('./routes/contactRoute'))
+*/
+app.use('/api/contacts',require('./routes/contactRoute'))
 app.listen(port, () => console.log('server is running at port...', port))
 
 
